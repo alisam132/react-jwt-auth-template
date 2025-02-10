e91ea0fc6f21cca241e7290e44025dbfb7070a2e
alisam132 <ali.habib132@gmail.com>
2025-02-10 18:58:14 +0300

    create react form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 // src/App.jsx
 
 import NavBar from './components/NavBar/NavBar';
+import { Routes, Route } from 'react-router';
+import SignUpForm from './components/SignUpForm/SignUpForm';
 
 const App = () => {
   
   return (
     <>
       <NavBar />
-      <h1>Hello, friend!</h1>
+      {/* Add the Routes component to wrap our individual routes*/}
+      <Routes>
+        <Route path='/sign-up' element={<SignUpForm />} />
+      </Routes>
     </>
   );
 };
